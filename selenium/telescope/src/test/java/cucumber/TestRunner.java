1cee577d248be404294cf4205301727dabf444ad
Ira <irunkalaytar@gmail.com>
2018-11-18 17:08:01 +0200

    removed not needed project

--- selenium/telescope/src/test/java/cucumber/TestRunner.java
+++
@@ -1,8 +0,0 @@
-package cucumber;
-
-import cucumber.api.CucumberOptions;
-import cucumber.api.testng.AbstractTestNGCucumberTests;
-
-@CucumberOptions(features = "src/test/resources/cucumber/features", glue = "cucumber/stepdefinitions")
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
